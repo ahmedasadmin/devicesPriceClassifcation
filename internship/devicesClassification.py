1475b9fe405a8fcc187e98629ea147c85c2e3d9e
Ahmed Muhammed Abdelgaber <87916181+ahmedasadmin@users.noreply.github.com>
2024-05-03 14:12:17 +0300

    Add files via upload

--- internship/devicesClassification.py
+++ internship/devicesClassification.py
@@ -1,16 +1,22 @@
+#########################################################################
+# Prices Classification of Devices Based on its SPecs 
+# author: Ahmed Muhammed Abdelgaber
+##########################################################################
 import numpy as np # linear algebra
 import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
 import matplotlib.pyplot as plt # for data visualization
 import seaborn as sns # for statistical data visualization
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
-import SVC classifier
-from sklearn.svm import SVC
+from sklearn.svm import SVC                   # import SVC classifier
 from sklearn.metrics import accuracy_score   # import metrics to compute accuracy
-
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
+from sklearn.metrics import classification_report
+#############################################################################
 train_data = pd.read_csv("train.csv")
 test_data = pd.read_csv("test.csv")
 
+target_labels = ['low cost', 'medium cost', 'high cost', 'very high cost']
 print(train_data)
 print(train_data.columns)
 print(train_data['price_range'].value_counts())          # check if dataset balanced or not 
@@ -38,11 +44,21 @@ X_test = scaler.transform(X_test)
 
 
                                                         # instantiate classifier with default hyperparameters
-svc=SVC() 
+svc=SVC(kernel='linear', degree=3, random_state=0) 
                                                         # fit classifier to training set
 svc.fit(X_train,y_train)
-                                                        # make predictions on test set
-y_pred=svc.predict(X_test)
+test_data_values = test_data.values                                                        # make predictions on test set
+reshaped_test_data = test_data_values.reshape((-1,20))
+y_pred=svc.predict(X_test)  
+print(classification_report(y_test, y_pred, labels=[0, 1, 2, 3]))
+# Prediction for first 10 devices 
+y_pred10 = svc.predict(reshaped_test_data[:10])
+y_pred10 = map(lambda x: target_labels[x], y_pred10)
+# confussion matrix 
+cm = confusion_matrix(y_test, y_pred, labels=svc.classes_)
+disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=svc.classes_)
+print('Model accuracy score with rbf kernel: {0:0.4f}'. format(accuracy_score(y_test, y_pred))) # compute and print accuracy score
+print("first 10 devices test :", list(y_pred10))
 
-                                                        # compute and print accuracy score
-print('Model accuracy score with rbf kernel and C=100.0 : {0:0.4f}'. format(accuracy_score(y_test, y_pred)))
+disp.plot()
+plt.show()
